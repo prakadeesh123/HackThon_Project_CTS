eea1dba4f14bfcbbe22a8126c8748d0c0b3a8257
2481712 <sagarreddy2812@gmail.com>
2026-05-01 12:51:57 +0530

    Testcases by sagar

--- src/test/java/org/MakeMyTripTest/com/Sagar/TC_02_Guestrating.java
+++ src/test/java/org/MakeMyTripTest/com/Sagar/TC_02_Guestrating.java
@@ -3,18 +3,23 @@ package org.MakeMyTripTest.com.Sagar;
 import org.ClearTrip.com.baseclass.BaseClass;
 import org.ClearTrip.com.pages.Sagar.GuestRating;
 import org.ClearTrip.com.pages.Sagar.Ratingfilter;
+import org.ClearTrip.com.pages.Yaswanth.FilterHotelsInNiarobia;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TC_02_Guestrating extends BaseClass {
     @Test
     public void testclass01(){
-        GuestRating gr=new GuestRating(driver);
-        gr.GuestRating();
-        Assert.assertTrue(gr.validaterating(),"Filter not applied");
+        FilterHotelsInNiarobia fh=new FilterHotelsInNiarobia(driver);
+        fh.filterHotel();
+        Assert.assertTrue(fh.ValidateFilter(),"Hotel not selected for nairobi");
         Ratingfilter pg=new Ratingfilter(driver);
         pg.apply_filter();
         Assert.assertTrue(pg.validaterating(),"Filter not applied");
+        GuestRating gr=new GuestRating(driver);
+        gr.GuestRating();
+        Assert.assertTrue(gr.validaterating(),"Filter not applied");
+
     }
 
 
