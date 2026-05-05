f7eb110d29547fab45846390f80381ad0a526b64
2481335 <prakadeesh04@gmail.com>
2026-05-05 09:51:39 +0530

    5 testcases changesss

--- src/test/java/org/ClearTrip/com/HotelBookingFiltersTest/TC_04_Propertytype.java
+++ src/test/java/org/ClearTrip/com/HotelBookingFiltersTest/TC_04_Propertytype.java
@@ -2,6 +2,7 @@ package org.ClearTrip.com.HotelBookingFiltersTest;
 
 import org.ClearTrip.com.baseclass.BaseClass;
 import org.ClearTrip.com.pages.HotelBookingFilters.Propertytype;
+import org.ClearTrip.com.pages.HotelBookingFunctsPrices.FilterHotelsInNiarobia;
 import org.testng.annotations.Test;
 
 public class TC_04_Propertytype extends BaseClass {
