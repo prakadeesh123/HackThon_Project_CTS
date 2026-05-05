51c30bf8d08770d1c6eb7f0150750a8ab1594840
2481712 <sagarreddy2812@gmail.com>
2026-05-05 14:35:22 +0530

    changes

--- src/main/java/org/ClearTrip/com/pages/HotelBookingFilters/GuestRating.java
+++ src/main/java/org/ClearTrip/com/pages/HotelBookingFilters/GuestRating.java
@@ -6,15 +6,20 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 
 
 public class GuestRating {
+
     public WebDriver driver;
     public WebDriverWait wait;
-//        private static final Logger log = LogManager.getLogManager().getLogger(GuestRating.class);
+
+    public static final Logger log= LoggerFactory.getLogger(GuestRating.class);
+
     @FindBy(xpath = "//p[text()='Guest ratings']")
     private WebElement guestrating;
 
@@ -31,18 +36,29 @@ public class GuestRating {
         this.driver=driver;
         this.wait=new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver , this);
+        log.info("Guest Rating page is initialized");
     }
     public void GuestRating(){
         try {
+
             wait.until(ExpectedConditions.elementToBeClickable(guestrating)).click();
+            log.info("Guest Rating dropdown opened");
+
             wait.until(ExpectedConditions.elementToBeClickable(highrating)).click();
+            log.info("High Guest rating checkbox has selected");
+
             wait.until(ExpectedConditions.elementToBeClickable(applybtn)).click();
+            log.info("High Guest rating filter is applied");
+
         } catch (Exception e) {
+
             e.printStackTrace();
+
         }
     }
     public boolean validaterating(){
         return wait.until(ExpectedConditions.visibilityOf(filterapplied)).isDisplayed();
+
 
     }
 }
